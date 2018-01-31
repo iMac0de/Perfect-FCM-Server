0f1427960a285cb9eb081fde3dd40154afc0b073
iMac0de <contact@jeremy-peltier.com>
2018-01-31 17:42:51 +0100

    Start implementation

--- Sources/Perfect-FCM-Server/PerfectFCM.swift
+++ Sources/Perfect-FCM-Server/PerfectFCM.swift
@@ -1,7 +1,24 @@
 import Foundation
 import OAuth2
+import cURL
+import PerfectCURL
 
 public class PerfectFCM {
+    
+    public enum Exception: Error {
+        case UnknownHost
+        case InvalidFile
+        case InvalidHeader
+        case CannonConvertToJson
+        case CannotSign
+        case CannotGetAccessToken
+    }
+    
+    static var access: Access?
+    
+    public static var token: Token?
+    
+    public static var debug = false
     
 	open class Config {
 		var serverKeyPath: String?
@@ -11,16 +28,99 @@ public class PerfectFCM {
 		var accessToken: String
 		var refreshToken: String
 		
+        
 		init(_ accessToken: String, _ refreshToken: String) {
 			self.accessToken = accessToken
 			self.refreshToken = refreshToken
 		}
 	}
 	
-	public class func prepare() {
-		if let path = ProcessInfo.processInfo.environment["GOOGLE_APPLICATION_CREDENTIALS"] {
-			print(path)
-		}
+	public class func prepare() throws -> CURLRequest {
+        let scope: [String] = ["https://www.googleapis.com/auth/firebase.messaging"]
+        
+        if let provider = DefaultTokenProvider(scopes: scope) {
+            let sem = DispatchSemaphore(value: 0)
+            
+            try? provider.withToken() { (token, error) -> Void in
+                if let token = token {
+                    self.token = token
+                }
+                if let error = error {
+                    print("ERROR \(error)")
+                }
+                sem.signal()
+            }
+            _ = sem.wait(timeout: DispatchTime.distantFuture)
+            
+            let curlRequest = CURLRequest("https://fcm.googleapis.com/v1/projects/elyot-fc163/messages:send", .failOnError)
+            curlRequest.addHeader(.authorization, value: "Bearer \(self.token?.AccessToken ?? "")")
+            curlRequest.addHeader(.contentType, value: "application/json")
+            
+            return curlRequest
+        } else {
+            print("Unable to obtain an auth token.\nTry pointing GOOGLE_APPLICATION_CREDENTIALS to your service account credentials.")
+            throw Exception.CannotGetAccessToken
+        }
 	}
+    
+    public class func send(topic: String, title: String, body: String, data: [String: String]?) throws {
+        var json: [String: Any] = [
+            "message": [
+                "topic": topic,
+                "notification": [
+                    "title": title,
+                    "body": body
+                ]
+            ]
+        ]
+        
+        if let data = data {
+            json["data"] = data
+        }
+        
+        let jsonData = try JSONSerialization.data(withJSONObject: json, options: [])
+        
+        guard let jsonString = String(data: jsonData, encoding: .utf8) else {
+            throw Exception.CannonConvertToJson
+        }
+        
+        let curlRequest = try prepare()
+        curlRequest.options.append(.httpMethod(.post))
+        curlRequest.options.append(.postString(jsonString))
+        
+        let curlResponse = try curlRequest.perform()
+        
+        print(curlResponse.bodyString)
+    }
+    
+    public class func send(token: String, title: String, body: String, data: [String: String]?) throws {
+        var json: [String: Any] = [
+            "message": [
+                "token": token,
+                "notification": [
+                    "title": title,
+                    "body": body
+                ]
+            ]
+        ]
+        
+        if let data = data {
+            json["data"] = data
+        }
+        
+        let jsonData = try JSONSerialization.data(withJSONObject: json, options: [])
+        
+        guard let jsonString = String(data: jsonData, encoding: .utf8) else {
+            throw Exception.CannonConvertToJson
+        }
+        
+        let curlRequest = try prepare()
+        curlRequest.options.append(.httpMethod(.post))
+        curlRequest.options.append(.postString(jsonString))
+        
+        let curlResponse = try curlRequest.perform()
+        
+        print(curlResponse.bodyString)
+    }
     
 }
--- Tests/Perfect-FCM-ServerTests/Perfect_FCM_ServerTests.swift
+++ Tests/Perfect-FCM-ServerTests/Perfect_FCM_ServerTests.swift
@@ -4,9 +4,13 @@ import XCTest
 class Perfect_FCM_ServerTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
+        // Use XCTAssert andz related functions to verify your tests produce the correct
         // results.
-		PerfectFCM.prepare()
+        do {
+            try PerfectFCM.send(token: "cNiYo3ya7Lk:APA91bGrBY1OURkyD6zf-taMOIyYyx_F54EigWWhltQnsvbCI38iYztVfoGy-PAMNnQunf_hiFF07p--qoukhxCjrtY0yZZUOl-c_mYZLscmLIVJFh1T1sV7dxi8PQsku7PYwR1TQTGo", title: "Hello world!", body: "First PUSH notification!", data: nil)
+        } catch {
+            print(error.localizedDescription)
+        }
 	}
 
 
