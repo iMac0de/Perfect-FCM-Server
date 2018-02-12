dcf84d038f201fe4a5f07109dca9555525336ce8
Jérémy Peltier <jeremy.peltier@epitech.eu>
2018-02-12 19:33:20 +0100

    Bug fix for Linux

--- Sources/Perfect-FCM-Server/PerfectFCM.swift
+++ Sources/Perfect-FCM-Server/PerfectFCM.swift
@@ -58,7 +58,7 @@ open class PerfectFCM {
             let curlResponse = try curlRequest.perform()
 			
 			if debug {
-				NSLog("%@", curlResponse.bodyString)
+				NSLog("%s", curlResponse.bodyString)
 			}
         }
     }
